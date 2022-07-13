4bd6f8de9bf383037a856032b2f1f234fcfe9ab5
Bayu Nugroho <91040581+bayunugroho2022@users.noreply.github.com>
2022-07-13 21:58:15 +0700

    update api

--- beli_rumah.php
+++ beli_rumah.php
@@ -6,7 +6,6 @@ header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// INCLUDING DATABASE AND MAKING OBJECT
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
